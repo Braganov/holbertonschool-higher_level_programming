9566834a9eb0dbc2c5bf1db4634e242fa531d964
Braga <ibrahim.houmaidi@gmail.com>
2025-01-26 15:02:24 +0100

    add function that prints x elements of a list

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    copy = 0
+    try:
+        for i in my_list:
+            if copy < x:
+                print(f"{my_list[copy]}", end="")
+                copy += 1
+        print()
+    except (TypeError):
+        pass
+    return copy
