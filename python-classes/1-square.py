db4fcfdac3483f118bf4915cecea6fa605d27fbf
Braga <ibrahim.houmaidi@gmail.com>
2025-01-29 14:10:30 +0100

    add class Square

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" the main class module """
+
+
+class Square:
+    """ the class square """
+    def __init__(self, size):
+        """
+        init function to initialize the square
+
+        Args:
+        size (int): the size of square
+        """
+        self.__size = size
