ea96211dc939c0b27ecec0e68e5b327b35e2a90d
Braga <ibrahim.houmaidi@gmail.com>
2025-02-09 13:36:51 +0100

    add The Mystical Dragon - Mastering Mixins

---
+++ python-abc/main_05_dragon.py
@@ -0,0 +1,6 @@
+from task_05_dragon import Dragon
+
+dragon = Dragon()
+dragon.swim()  # Outputs: The creature swims!
+dragon.fly()   # Outputs: The creature flies!
+dragon.roar()  # Outputs: The dragon roars!
