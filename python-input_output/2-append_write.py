8c20717a1b04980104a3f230ddc1cffad4840a6f
Braga <ibrahim.houmaidi@gmail.com>
2025-02-10 15:04:39 +0100

    update code

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """ defining append_write function """
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """ append a string to a text file (UTF8) and returns the number of characters written """
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
