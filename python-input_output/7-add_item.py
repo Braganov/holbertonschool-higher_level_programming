0761fea6a61e10d7c892dde03d3a0d55495487f9
Braga <ibrahim.houmaidi@gmail.com>
2025-02-11 10:32:53 +0100

    update task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 """ task 7 module"""
 
+import json
 import sys
+
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 arglist = list(sys.argv[1:])
 
 try:
-    my_list = load_from_json_file('add_item.json')
-except Exception:
+    old_data = load_from_json_file('add_item.json')
+except FileNotFoundError:
     old_data = []
 
 old_data.extend(arglist)
-save_to_json_file(old_data, 'add_item.json')
+save_to_json_file(old_data, "add_item.json")
