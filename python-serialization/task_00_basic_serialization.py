9a91516dbb21fdf94380614ec5450cf71b4fb565
Braganov <ibrahim.houmaidi@gmail.com>
2025-02-16 10:44:05 +0100

    add basic serialization module

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+"""
+Module for basic serialization and deserialization
+of a Python dictionary.
+"""
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """
+    Serializes a Python dictionary to a JSON file.
+
+    Args:
+        data (dict): The dictionary to serialize.
+        filename (str): The filename of the JSON file.
+
+    Returns:
+        None
+    """
+    with open(filename, "w") as file:
+        json.dump(data, file)
+
+
+def load_and_deserialize(filename):
+    """
+    Loads a JSON file and deserializes it back into a Python dictionary.
+
+    Args:
+        filename (str): The filename of the JSON file.
+
+    Returns:
+        dict: The deserialized dictionary.
+    """
+    with open(filename, 'r') as file:
+        data = json.load(file)
+    return data
