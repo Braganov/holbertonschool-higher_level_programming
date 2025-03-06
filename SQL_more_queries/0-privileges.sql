6d11f08a5d1d80bca4964e3e027f4146f8e75a9f
Braga <ibrahim.houmaidi@gmail.com>
2025-03-06 09:44:02 +0100

    task 0

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost)
+-- user_0d_1 should have only SELECT privilege in the database hbtn_0d_2
+-- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
