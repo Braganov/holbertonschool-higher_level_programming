6fae6401e258af4b80c09ef80e6ed7ab035b3d69
Braga <ibrahim.houmaidi@gmail.com>
2025-03-09 13:02:50 +0100

    update code task 10

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 """
-script that changes the name of a State object from the database hbtn_0e_6_usa
+Write a script that prints the State object with the name passed as argument from the database hbtn_0e_6_usa
 
+Your script should take 4 arguments: mysql username, mysql password, database name and state name to search (SQL injection free)
+You must use the module SQLAlchemy
+You must import State and Base from model_state - from model_state import Base, State
+Your script should connect to a MySQL server running on localhost at port 3306
+You can assume you have one record with the state name to search
+Results must display the states.id
+If no state has the name you searched for, display Not found
+Your code should not be executed when imported
 """
+
 from sys import argv
-from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 if __name__ == "__main__":
     user, passwd, database = argv[1], argv[2], argv[3]
@@ -15,9 +24,9 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).filter(State.id == 2).first()
-    state.name = "New Mexico"
-    session.commit()
+    state = session.query(State).filter(State.name == argv[4]).first()
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
     session.close()
-
-    engine.dispose()
