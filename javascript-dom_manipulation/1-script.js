81274223d23852f2f64e9ade7d653727f6d29d89
Braga <ibrahim.houmaidi@gmail.com>
2025-04-04 14:10:43 +0200

    task 1

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,4 @@
+// Script that updates the text color of the header element to red when user clicks on red_header
+document.getElementById('red_header').addEventListener('click', function () {
+  document.querySelector('header').style.color = '#FF0000';
+});
